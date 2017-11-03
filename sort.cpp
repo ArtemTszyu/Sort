33fde2b7a92ff226fdcca2765f1b395ee3edbeb5
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2017-11-03 11:28:06 +0300

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,69 +1,66 @@
 #include <iostream>
 #include <sstream>
 #include <utility>
-#include <stdlib.h>
 using namespace std;
 
 bool str(float *st,int n)
 {
-	bool h=true;
-	string t;
-	getline(cin,t);
-	istringstream stream(t);
-	for(int i = 0; i<n; i++)
-	{
-	   if(!(stream>>st[i]))
-	      {h = false;
-	       return false;}
-	}
-   return true;
- } 
- 
-void sort (float* st, int n, int e) 
- {
-   for (unsigned int i=0; i<n; i++) 
-	{
-	  for (unsigned j=i+1; j<n; j++) 
-		{
-		  if (st[i] > st[j])
-			{
-			 swap (st[i], st[j]);
-			 e=e+5;
-			}
-		}
-			
-		}
-		if (e==0){cout << "There is nothing to swap";exit (0);
-	}
-	
+    bool h=true;
+    string t;
+    getline(cin,t);
+    istringstream stream(t);
+    for(unsigned int i = 0; i<n; i++)
+    {
+        if(!(stream>>st[i]))
+        {
+            h = false;
+            return false;
+        }
+    }
+    return true;
 }
- void print_st(float * st, unsigned int n)
-{
-	for (unsigned int i = 0; i < n; i++)
-	{
-		cout << st[i] << " ";
-	}
-}
- int main()
- {
 
-unsigned int n;
-int e;
-cin>>n;
-cin.get();
-float *st = new float[n];
-if(str(st, n))
-	{ 
-		sort(st, n, e);
-		print_st(st, n);
-		cin.get();
-		}
-	  else
-	{
-		cout << "An error has occured while reading numbers from line";
-		cin.get();
-	}
-	 delete st;
-        return 0;
-	}	
- 
+void sort (float* st, int n)
+{
+    for (unsigned int i=0; i<n; i++)
+    {
+        for (unsigned j=i+1; j<n; j++)
+        {
+            if (st[i] > st[j])
+            {
+                swap (st[i], st[j]);
+            }
+        }
+
+    }
+
+
+
+}
+void print_st(float * st, unsigned int n)
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
+        cout << st[i] << " ";
+    }
+}
+int main()
+{
+
+    unsigned int n;
+    cin>>n;
+    cin.get();
+    float *st = new float[n];
+    if(str(st, n))
+    {
+        sort(st, n);
+        print_st(st, n);
+    }
+    else
+    {
+        cout << "An error has occured while reading numbers from line";
+    }
+    delete st;
+    return 0;
+}
+
