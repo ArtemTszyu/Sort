f03a3de803c46242901bd1a234a5182d28430afe
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2017-11-03 11:37:07 +0300

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -37,6 +37,7 @@ void sort (float* st, int n)
 
 
 }
+
 void print_st(float * st, unsigned int n)
 {
     for (unsigned int i = 0; i < n; i++)
@@ -44,6 +45,7 @@ void print_st(float * st, unsigned int n)
         cout << st[i] << " ";
     }
 }
+
 int main()
 {
 
